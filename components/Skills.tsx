b6e56c0e2b562a90ba737e1404623357e1175c4c
Patrik Tao <patriktao@gmail.com>
2023-09-27 18:22:47 +0200

    Update

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,32 +13,45 @@ const Skills = (props: Props) => {
       className="grid justify-start lg:justify-center text-center mt-44 md:mt-0"
     >
       <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-12 lg:mx-0 lg:max-w-none lg:grid-cols-1 lg:gap-y-10">
-          <Fade duration={1000} triggerOnce cascade damping={0.1}>
+        <Fade duration={1000} triggerOnce cascade damping={0.1}>
           <h1 className="text-5xl md:text-7xl font-bold text-center md:text-left lg:text-center bg-gradient-to-r from-green-400 to-[--color-theme] bg-clip-text text-transparent">
             Skills
           </h1>
           <div className="flex-wrap flex gap-y-8 gap-x-12">
             <SkillList
-              header="Front-end"
+              header="Project & Product"
               text={
                 <ul>
-                  <li>Javascript (React, Next.js, React Native)</li>
-                  <li>Typescript</li>
-                  <li>HTML/CSS (SCSS, Tailwind)</li>
-                  <li>Tailwind</li>
+                  <li>Agile Scrum</li>
+                  <li>Kanban</li>
+                  <li>Product Strategy</li>
+                  <li>UX Design</li>
+                  <li>Requirement Engineering</li>
+                  <li>Configuration Management</li>
+                </ul>
+              }
+              icon={<FaCube />}
+            />
+            <SkillList
+              header="Frontend"
+              text={
+                <ul>
+                  <li>Javascript (Typescript)</li>
+                  <li>React (Native, NextJs)</li>
+                  <li>HTML/CSS/Tailwind</li>
                   <li>Redux</li>
                 </ul>
               }
               icon={<BsCodeSquare />}
             />
             <SkillList
-              header="Back-end"
+              header="Backend"
               text={
                 <ul>
                   <li>Java</li>
-                  <li>Javascript (Node.js, Express.js)</li>
-                  <li>SQL (PostgreSQL)</li>
+                  <li>NodeJs (Express)</li>
                   <li>Python (Django)</li>
+                  <li>PostgreSQL</li>
                   <li>Firebase</li>
                   <li>AWS S3</li>
                 </ul>
@@ -49,12 +62,11 @@ const Skills = (props: Props) => {
               header="Data Science"
               text={
                 <ul>
-                  <li>R</li>
-                  <li>Haskell</li>
                   <li>Python</li>
-                  <li>Clojure</li>
-                  <li>MatLab</li>
+                  <li>R</li>
                   <li>Excel</li>
+                  <li>MatLab</li>
+                  <li>Haskell</li>
                 </ul>
               }
               icon={<BsClipboardDataFill />}
@@ -65,30 +77,12 @@ const Skills = (props: Props) => {
               text={
                 <ul>
                   <li>Docker</li>
-                  <li>Git</li>
+                  <li>Git/Github</li>
                   <li>Figma</li>
-                  <li>PowerPoint</li>
-                  <li>Webpack</li>
                   <li>Wordpress</li>
-                  <li>Heroku</li>
+                  <li>Webpack</li>
                 </ul>
               }
-            />
-            <SkillList
-              header="Coursework"
-              text={
-                <ul>
-                  <li>System Design</li>
-                  <li>Testing</li>
-                  <li>Scrum</li>
-                  <li>Parallell Programming</li>
-                  <li>UX Design</li>
-                  <li>Requirement Eng.</li>
-                  <li>Configuration Mgm. (SCM)</li>
-                  <li>Agile Scrum</li>
-                </ul>
-              }
-              icon={<FaCube />}
             />
           </div>
         </Fade>
